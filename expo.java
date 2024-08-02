a1d49ac37e5d1efb100210cb4cc1722ddf48d944
gautamrizwani <gautamrizwani@gmail.com>
2024-08-02 18:41:11 +0530

    uaaaaaanamaste

--- expo.java
+++ expo.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class expo {
     public static void main(String[] args) {
-        System.out.println("hello world kismat thukso");
+        System.out.println("hello world kismat thukso mukdo");
     }
 }
